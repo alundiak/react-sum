c0f2453564e90330c0b80eaf29ce3a89d03cf9e4
Andrii Lundiak <landike@gmail.com>
2019-03-31 22:06:35 +0200

    Modified code to a+b form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
     state = {
-
+        // TODO
     }
 
     render() {
+        const { a, b } = this.props;
+        const result = a + b;
 
         return (
-            <React.Fragment>
-                <h2>ReactJS npm package</h2>
-                <div className="app-container">
-                    TODO
-                </div>
-            </React.Fragment>
+            <div>
+                <h1>{result}</h1>
+            </div>
         );
     }
 }
+
+App.propTypes = {
+    a: PropTypes.number,
+    b: PropTypes.number
+};
+
+App.defaultProps = {
+    a: 2,
+    b: 2
+};
 
 export default App;
