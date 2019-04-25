ad1ea723bf44cebd6b7e1394464a2c41f28c1269
Andrii Lundiak <landike@gmail.com>
2019-04-25 17:49:56 +0200

    Temp

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
 import { Sum } from 'components/App';
-import { SumWithCssAlias } from 'components/App';
+// import { SumWithCssAlias } from 'components/App';
 import 'css/main.less';
 
 const Main = () => (
   <React.Fragment>
-    <Sum />
-    <SumWithCssAlias />
+    {/* <Sum /> */}
+    {/* <SumWithCssAlias /> */}
 
     <App.Sum a={3} b={3} />
     <App.SumWithCssAlias a={4} b={4} />
