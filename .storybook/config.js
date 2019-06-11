79fde341e5a2450f11cacfdf4bafe247832b0fc3
Andrii Lundiak <landike@gmail.com>
2019-06-12 01:00:53 +0200

    Added Sorybook Static code, to try on GitHub Pages.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
+// https://webpack.js.org/guides/dependency-management/#require-context
 // TODO play more with context. It might be alternative approach to `config.resolve.modules`
 const req = require.context('../stories', true, /\.stories\.js?$/);
 function loadStories() {
