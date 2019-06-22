6cc18fd3dcc5b7cb0bb86bffe324dec974e5f767
Andrii Lundiak <landike@gmail.com>
2019-06-22 20:35:37 +0200

    Modified Storybook changes. Updated dependencies.

--- stories/sum.stories.js
+++ stories/sum.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
+import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
 
 import Sum from '../src/components/Sum';
@@ -9,17 +9,28 @@ import SumWithCssAlias from 'components/SumWithCssAlias';
 // import SumWithAscii from 'components/SumWithAscii';
 import DangMath from 'components/DangMath';
 
-storiesOf('Sum', module)
-  .add('2+2 (default)', () => <Sum />)
+import App from 'components/App';
+
+import { version } from '../package';
+
+const group = `Component (v${version})|`;
+
+storiesOf(group + 'Sum', module)
+  .add('2+2 (default)', () => <Sum onClick={action('clicked')} />)
   .add('custom numbers', () => <Sum a={3} b={3} />)
   .add('negative numbers', () => <Sum a={3} b={-2} />)
   .add('negative numbers (=> 0)', () => <Sum a={3} b={-3} />)
   .add('useImages', () => <Sum a={4} b={4} useImages />)
   .add('useASCII', () => <Sum a={5} b={5} useASCII />);
 
-storiesOf('Sum with alias', module)
+storiesOf(group + 'Sum with alias', module)
   .add('default logic', () => <SumWithCssAlias a={6} b={6} />)
   .add('custom numbers', () => <SumWithCssAlias a={7} b={7} />);
 
-storiesOf('DangMath', module)
+storiesOf('Other|Dang Math', module)
   .add('simple usage', () => <DangMath a={8} b={8} c={16} />);
+
+storiesOf('Other|App.Sum')
+  .add('2+2', () => <App.Sum />)
+storiesOf('Other|App.SumWithCssAlias')
+  .add('2+2', () => <App.SumWithCssAlias />)
