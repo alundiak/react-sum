829718d203590a169ec0ff77e9a28fbdcef5d902
Andrii Lundiak <landike@gmail.com>
2019-06-23 19:30:52 +0200

    Added AppVeyor CI integration config file.

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,8 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-docs/register';
+
+// register the notes addon as a tab
+import '@storybook/addon-notes/register';
+// or register the notes addon as a panel. Only one can be used!
 import '@storybook/addon-notes/register-panel';
